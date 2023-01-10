77f28907fa628934130ace864ee5f9c47986724c
jstted <tedeev17@gmail.com>
2023-01-10 18:30:53 +0300

    Added notes for add screen

--- NavController test/Controllers/Home/AddReminderViewController.swift
+++ NavController test/Controllers/Home/AddReminderViewController.swift
@@ -10,17 +10,18 @@ import UIKit
 class AddReminderViewController: UIViewController {
 
     var reminderTextField = UITextField()
-    var noteTextField = UITextView()
+    var noteTextView = UITextView()
     var saveButton = UIButton()
     var cancelButton = UIButton()
     var safeArea: UILayoutGuide!
 
+    //MARK: - ViewDidLoad
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         setupReminderTextField()
-        setupNoteTextField()
+        setupNoteTextView()
         setupSaveButton()
         safeArea = view.layoutMarginsGuide
         navigationItem.title = "hi"
@@ -33,7 +34,7 @@ class AddReminderViewController: UIViewController {
         view.addSubview(saveButton)
         
         saveButton.setTitle("Save", for: .normal)
-        saveButton
+        saveButton.titleLabel?.font = .boldSystemFont(ofSize: 17)
         saveButton.setTitleColor(.black, for: .normal)
         saveButton.setTitleColor(.darkGray, for: .highlighted)
         
@@ -61,21 +62,27 @@ class AddReminderViewController: UIViewController {
 
     }
     
-    private func setupNoteTextField() {
+    private func setupNoteTextView() {
         
-        view.addSubview(noteTextField)
         
-        noteTextField.isEditable = true
-        noteTextField.backgroundColor = .black
-        noteTextField.text = """
+        
+        noteTextView = UITextView(frame: .zero)
+        noteTextView.isEditable = true
+        noteTextView.textColor = .black
+        noteTextView.
+        noteTextView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        noteTextView.text = """
 Terminating app due to uncaught exception 'NSGenericException', reason: 'Unable to activate constraint with anchors and because they have no common ancestor. Does the constraint or its anchors reference items in different view hierarchies? That's illegal.'
 """
         
-        noteTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(noteTextView)
+        noteTextView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            noteTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 16),
-            noteTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            noteTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+            noteTextView.topAnchor.constraint(equalTo: reminderTextField.bottomAnchor, constant: 16),
+            noteTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            noteTextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            noteTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 16)
         ])
     }
         
